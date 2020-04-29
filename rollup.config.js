360e61637cbed4f709232c7da9dc0bcb188112e3
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-04-29 14:46:23 +0300

    build fix unresolved deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import typescript from 'rollup-plugin-typescript2';
+import pkg from "./package.json";
 
 export default [
   {
     input: './src/OpenAPI.ts',
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+      'url',
+      'events',
+    ],
     output: {
       file: 'build/OpenAPI.js',
       format: 'cjs',
