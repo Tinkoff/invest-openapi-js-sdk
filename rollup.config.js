be08877b66b779c921aa926d8964750c9edc94da
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-05-09 00:34:51 +0300

    Современная сборка esm + cjs

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import typescript from 'rollup-plugin-typescript2';
 export default [
   {
     input: './src/OpenAPI.ts',
-    output: {
-      file: 'build/OpenAPI.js',
-      format: 'cjs',
-    },
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "esm" }
+    ],
     plugins: [typescript()],
   },
 ];
