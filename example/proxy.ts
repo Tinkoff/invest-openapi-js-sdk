338e2e9a5989c7aa47a8f61776dace6fd19ed9b8
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-05-09 09:38:46 +0300

    proxy

---
+++ example/proxy.ts
@@ -0,0 +1,46 @@
+/**
+ * Утилита для проксирования соединений в одно
+ */
+import WebSocket, { Server } from 'ws';
+
+const token = 'MY_TOKEN';
+const socketURL = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws';
+const wss = new Server({ port: 8080 });
+const enum ReadyState {
+    'CONNECTING',
+    'OPEN',
+    'CLOSING',
+    'CLOSED',
+}
+const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' };
+let targetWs = new WebSocket(socketURL, { perMessageDeflate: false, headers });
+
+wss.on('connection', (ws) => {
+    if (targetWs && ![ReadyState.OPEN, ReadyState.CONNECTING].includes(targetWs.readyState)) {
+        targetWs.terminate();
+        targetWs = new WebSocket(socketURL, { perMessageDeflate: false, headers });
+    }
+
+    targetWs.on('message', (msg) => {
+        console.log('in: ', msg.slice(0, 20), '...');
+        ws.send(msg);
+    });
+    targetWs.on('close', () => ws.close());
+    targetWs.on('error', (error) => {
+        console.log(error);
+        ws.close();
+    });
+
+    ws.on('message', (msg: string) => {
+        console.log('out: ', msg.slice(0, 20), '...');
+        if (targetWs.readyState === ReadyState.OPEN) {
+            targetWs.send(msg);
+        }
+    });
+
+    // TODO: Пока при каждом дисконнекте будет разрыв, чтобы обнулить подписки
+    // Можно подумать что сделать в данном случа
+    ws.on('close', () => {
+        targetWs.close();
+    });
+});
