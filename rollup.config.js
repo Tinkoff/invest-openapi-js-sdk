9bf2d75efb2e38355c4db79470d0bb6e7f95f31e
Dmitriy Yurov <dmitry.yurov@corp.mail.ru>
2020-05-09 18:47:17 +0300

    Современная сборка esm + cjs (#57)
    * Современная сборка esm + cjs
    
    * Стриминг экспорт
    
    * no mixed defaults export
    
    * missed pkg
    
    * target es2018
    
    * revert default export
    
    * setup protected
    
    * revert

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import typescript from 'rollup-plugin-typescript2';
+import pkg from "./package.json";
 
 export default [
   {
     input: './src/OpenAPI.ts',
-    output: {
-      file: 'build/OpenAPI.js',
-      format: 'cjs',
-    },
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "esm" }
+    ],
     plugins: [typescript()],
   },
 ];
