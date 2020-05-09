4e9f40d15d812d9b082037359009e3518c4681b1
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-05-09 18:12:11 +0300

    final proxy

--- example/proxy.ts
+++ example/proxy.ts
@@ -17,38 +17,72 @@ const enum ReadyState {
     'CLOSED',
 }
 const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' };
-let targetWs = new WebSocket(socketURL, { perMessageDeflate: false, headers });
+let pingTimeout: NodeJS.Timeout;
+let targetWs: WebSocket;
 
-wss.on('connection', (ws) => {
-    if (targetWs && ![ReadyState.OPEN, ReadyState.CONNECTING].includes(targetWs.readyState)) {
+/**
+ * Поддержание соединения
+ */
+function startProxy() {
+    if (targetWs) {
         targetWs.terminate();
-        targetWs = new WebSocket(socketURL, { perMessageDeflate: false, headers });
     }
 
-    targetWs.on('message', (msg) => {
-        console.log('in: ', msg.slice(0, 20), '...');
-        ws.send(msg);
-    });
-    targetWs.on('close', () => ws.close());
-    targetWs.on('error', (error) => {
-        console.log(error);
-        ws.close();
-    });
+    targetWs = new WebSocket(socketURL, { perMessageDeflate: false, headers });
+
+    clearTimeout(pingTimeout);
+}
+
+wss.on('connection', (ws) => {
+    if (!targetWs || (targetWs && ![ReadyState.OPEN, ReadyState.CONNECTING].includes(targetWs.readyState))) {
+        startProxy();
+    }
+
+    const handleMessage = (msg: string | Buffer) => {
+        console.log('in: ', msg.slice(0, 40), '...');
+        if (ws.readyState === ReadyState.OPEN) {
+            ws.send(msg);
+        }
+    };
+
+    const handleClose = (e: Event | number) => {
+        console.log('Remote server close connection\n', e);
+        if (ws.readyState === ReadyState.OPEN) {
+            ws.close();
+        }
+
+        // Отписываемся чтобы не было утечек памяти
+        targetWs.off('message', handleMessage);
+        targetWs.off('close', handleClose);
+        targetWs.off('error', handleClose);
+
+        ws.terminate();
+    };
+
+    targetWs.on('message', handleMessage);
+    targetWs.on('close', handleClose);
+    targetWs.on('error', handleClose);
 
     ws.on('message', (msg: string) => {
-        console.log('out: ', msg.slice(0, 20), '...');
+        console.log('out: ', msg.slice(0, 40), '...');
         if (targetWs.readyState === ReadyState.OPEN) {
             targetWs.send(msg);
         }
     });
 
-    /*
-        Чтобы бот при отключении не оставлял подписку просто так висеть
-        Отпишем его перед выходом
-
-        process.on('SIGINT', () => {
-            unsubscribe();
-            process.exit();
-        });
-     */
+    ws.on('ping', () => {
+        if (targetWs.readyState === ReadyState.OPEN) {
+            console.log('local in ping');
+        }
+    });
 });
+
+/*
+    Чтобы бот при отключении не оставлял подписку просто так висеть
+    Отпишем его перед выходом
+
+    process.on('SIGINT', () => {
+        unsubscribe();
+        process.exit();
+    });
+ */
